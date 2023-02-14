b6b97c55702153d38e6b5959b4df13c99c52193d
magusafr <40602197+magusafr@users.noreply.github.com>
2023-02-15 00:30:04 +0700

    Update rfm.sql

--- rfm.sql
+++ rfm.sql
@@ -58,47 +58,47 @@ ORDER BY AvgMonetaryValue DESC;
 
 -- create CTE to make readable codes
 WITH rfm_table AS (
-					SELECT
-						  store_name,
-						  SUM(sale_dollars) MonetaryValue,
-						  AVG(sale_dollars) AvgMonetaryValue,
-						  COUNT(*) frequency,
-						  MAX(date) last_order_date,
-						  (SELECT MAX(date) FROM iowa_drink_sales) max_order_date,
-						  (SELECT MAX(date) FROM iowa_drink_sales) - MAX(date) recency
-					FROM iowa_drink_sales
-					GROUP BY store_name
-					ORDER BY AvgMonetaryValue DESC
+		SELECT
+			  store_name,
+			  SUM(sale_dollars) MonetaryValue,
+			  AVG(sale_dollars) AvgMonetaryValue,
+			  COUNT(*) frequency,
+			  MAX(date) last_order_date,
+			  (SELECT MAX(date) FROM iowa_drink_sales) max_order_date,
+			  (SELECT MAX(date) FROM iowa_drink_sales) - MAX(date) recency
+		FROM iowa_drink_sales
+		GROUP BY store_name
+		ORDER BY AvgMonetaryValue DESC
                   ),
 rfm_cel AS (
-			SELECT *,
-				   ntile(4) over (order by last_order_date) as rfm_recency,
-				   ntile(4) over (order by frequency) as rfm_frequency,
-				   ntile(4) over (order by AvgMonetaryValue) as rfm_monetary
-			FROM rfm_table
+	SELECT *,
+		   ntile(4) over (order by last_order_date) as rfm_recency,
+		   ntile(4) over (order by frequency) as rfm_frequency,
+		   ntile(4) over (order by AvgMonetaryValue) as rfm_monetary
+	FROM rfm_table
 	        ),
 rfm_cel2 AS(
-			SELECT store_name,
-				   rfm_recency,
-				   rfm_frequency,
-				   rfm_monetary,
-				   rfm_recency + rfm_frequency + rfm_monetary as rfm_cell,
-				   CONCAT(CAST(rfm_recency AS varchar), CAST(rfm_frequency AS varchar), CAST(rfm_monetary as varchar)) rfm_cell_string
-			FROM rfm_cel
-			ORDER BY rfm_cell --min 3 and max 12
-			),
+	SELECT store_name,
+		   rfm_recency,
+		   rfm_frequency,
+		   rfm_monetary,
+		   rfm_recency + rfm_frequency + rfm_monetary as rfm_cell,
+		   CONCAT(CAST(rfm_recency AS varchar), CAST(rfm_frequency AS varchar), CAST(rfm_monetary as varchar)) rfm_cell_string
+	FROM rfm_cel
+	ORDER BY rfm_cell --min 3 and max 12
+	   ),
 rfm_segment AS (
-				select store_name , rfm_recency, rfm_frequency, rfm_monetary, rfm_cell_string,
-					case 
-						when CAST(rfm_cell_string AS integer) in (111, 113, 124, 112 , 121, 122, 123, 132, 211, 212, 114, 141, 213, 214) then '6_lost_customers'  --lost customers
-						when CAST(rfm_cell_string AS integer)  in (131, 133, 134, 142, 143, 244, 334, 343, 344, 144) then '5_slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
-						when CAST(rfm_cell_string AS integer)  in (311, 411, 331, 412, 413, 414, 421, 423, 424) then '4_new customers'
-						when CAST(rfm_cell_string AS integer)  in (222, 223, 233, 322, 221, 224, 231, 232, 234, 241, 242, 243) then '3_potential churners'
-						when CAST(rfm_cell_string AS integer)  in (323, 333, 321, 422, 332, 432, 312, 313, 314, 324, 341, 342) then '2_active' --(Customers who buy often & recently, but at low price points)
-						when CAST(rfm_cell_string AS integer)  in (433, 434, 443, 444, 431, 441, 442) then '1_loyal'
-					end rfm_segment
-				FROM rfm_cel2
-				)
+	SELECT store_name , rfm_recency, rfm_frequency, rfm_monetary, rfm_cell_string,
+		case 
+			when CAST(rfm_cell_string AS integer) IN (111, 113, 124, 112 , 121, 122, 123, 132, 211, 212, 114, 141, 213, 214) then '6_lost_customers'  --lost customers
+			when CAST(rfm_cell_string AS integer) IN (131, 133, 134, 142, 143, 244, 334, 343, 344, 144) then '5_slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+			when CAST(rfm_cell_string AS integer) IN (311, 411, 331, 412, 413, 414, 421, 423, 424) then '4_new customers'
+			when CAST(rfm_cell_string AS integer) IN (222, 223, 233, 322, 221, 224, 231, 232, 234, 241, 242, 243) then '3_potential churners'
+			when CAST(rfm_cell_string AS integer) IN (323, 333, 321, 422, 332, 432, 312, 313, 314, 324, 341, 342) then '2_active' --(Customers who buy often & recently, but at low price points)
+			when CAST(rfm_cell_string AS integer) IN (433, 434, 443, 444, 431, 441, 442) then '1_loyal'
+		end rfm_segment
+	FROM rfm_cel2
+		)
 
 SELECT rfm_segment, COUNT(*)
 FROM rfm_segment
